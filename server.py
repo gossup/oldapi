762350e254fed2b8ae9274e693309d8003c6976c
Billy Askea <billy@gossup.com>
2022-06-27 16:33:26 -0700

    30

--- server.py
+++ server.py
@@ -4,41 +4,32 @@ import json
 import http.client
 from collections import Counter
 from flask import Flask, render_template
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    url = os.getenv("URL")
-    return url
+    port = os.getenv("PORT")
+    return port
     
 @app.route('/next')
 def next():
-    url = os.getenv("URL")
-    parsed_url = urlparse(url)
-    captured_value = parse_qs(parsed_url.query)['name'][0]
-    return captured_value
-    
-@app.route('/url')
-def andagain():
-    url = os.environ['URL']
-    return url
-    
-@app.route('/url2')
-def andagain():
-    return __url__
+    result = ""
+    for i in sys.argv:
+        old = result
+        result = old + i
+    return result
     
 @app.route('/again')
 def again():
-    url = os.environ['HTTP_HOST']
-    uri = os.environ['REQUEST_URI']
-    return url + uri
+    n = __name__
+    return n
     
 @app.route('/andagain')
 def andagain():
-    return __args__
+    url = os.environ['HTTP_HOST']
+    uri = os.environ['REQUEST_URI']
+    return url + uri
 
 if __name__ == '__main__':
     app.run()
