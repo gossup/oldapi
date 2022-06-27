03e570c792a93edaa1558a868629a9fe27f43dcd
Billy Askea <billy@gossup.com>
2022-06-27 16:14:52 -0700

    28

--- server.py
+++ server.py
@@ -12,22 +12,28 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     url = os.getenv("URL")
-    parsed_url = urlparse(url)
-    captured_value = parse_qs(parsed_url.query)['name'][0]
-    return captured_value
+    return url
     
 @app.route('/next')
 def next():
-    result = ""
-    for item, value in os.environ.items():
-        old = result
-        result = old + '{}: {}'.format(item, value)
-    return result
+    url = os.getenv("URL")
+    parsed_url = urlparse(url)
+    captured_value = parse_qs(parsed_url.query)['name'][0]
+    
+@app.route('/url')
+def andagain():
+    url = os.environ['URL']
+    return url
+    
+@app.route('/url2')
+def andagain():
+    return __url__
     
 @app.route('/again')
 def again():
-    n = __name__
-    return n
+    url = os.environ['HTTP_HOST']
+    uri = os.environ['REQUEST_URI']
+    return url + uri
     
 @app.route('/andagain')
 def andagain():
