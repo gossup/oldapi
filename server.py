01e7b6d00491ad972f505811cfc5d278fc0f63ea
Billy Askea <billy@gossup.com>
2022-06-27 00:57:48 -0700

    Fifth Attempt

--- server.py
+++ server.py
@@ -4,9 +4,11 @@ import os
 
 PORT = 8080
 
-web_dir = os.path.join(os.path.dirname(__file__), 'web')
-os.chdir(web_dir)
+def main:
+    web_dir = os.path.join(os.path.dirname(__file__), 'web')
+    os.chdir(web_dir)
 
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    httpd.serve_forever()
+    return { 'hello': "bitches" }
