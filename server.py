33663068d4306cc1d2d36e5fa6cc8a824e3dc54c
Billy Askea <billy@gossup.com>
2022-06-27 12:03:29 -0700

    Ninth Attempt

--- server.py
+++ server.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def main():
     hello = "HELLO BITCH"
     return hello
+    
+@app.route('/next')
+def next():
+    hello = "HELLO NEXT"
+    return hello
 
 if __name__ == '__main__':
     app.run()
