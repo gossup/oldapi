7364d4aec7ce4bf90a7fbe7db9fc2080cb284a19
Billy Askea <billy@gossup.com>
2022-06-27 12:27:16 -0700

    Eleventh Attempt

--- server.py
+++ server.py
@@ -4,13 +4,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    hello = "HELLO BITCH"
+def main(args):
+    bitch = args.get('name')
+    name = "BITCH"
+    if bitch:
+        name = bitch
+    hello = "HELLO {0}".format(name)
     return hello
     
 @app.route('/next')
 def next():
-    hello = os.env.get('XXX')
+    hello = os.getenv('XXX')
     return hello
 
 if __name__ == '__main__':
