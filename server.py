edbdd9c3fdb8a7e722e3a779ee3cdbc56260ec21
Billy Askea <billy@gossup.com>
2022-06-28 01:04:48 -0700

    60

--- server.py
+++ server.py
@@ -33,14 +33,14 @@ def next():
     if not since:
         return { 'Error': "Missing since." }
         
-#    split = since.split(',')
-#
-#    createdAt = "'{0} {1}'".format(split[0], split[1])
+    split = since.split(',')
 
-    return { 'message': since }
+    createdAt = "'{0} {1}'".format(split[0], split[1])
+
+#    return { 'message': since }
                 
-    getPostsCommand = "SELECT p.parentId AS parentId, p.createdBy AS createdBy FROM GOSSUP.post p WHERE p.createdAt > {0} GROUP BY parentId, createdBy ORDER BY p.createdBy;".format(since)
-
+    getPostsCommand = "SELECT p.parentId AS parentId, p.createdBy AS createdBy FROM GOSSUP.post p WHERE p.createdAt > {0} GROUP BY parentId, createdBy ORDER BY p.createdBy;".format(createdAt)
+    
     getPostsSqlCommand = {
     'commands': getPostsCommand,
     'limit': 1000,
@@ -58,10 +58,10 @@ def next():
     conn = http.client.HTTPSConnection(hostName)
     
     conn.request("POST", "/dbapi/v4/sql_jobs", headers=headers, body=json.dumps(getPostsSqlCommand))
-
+    
     postRes = conn.getresponse()
     postData = postRes.read()
-
+    
     transactionID = json.loads(postData.decode("utf-8")).get('id')
     
     conn.request("GET", "/dbapi/v4/sql_jobs/{}".format(transactionID), headers=headers)
@@ -83,12 +83,12 @@ def next():
     for row in rows:
         createdById = row[1]
         createdByIds.append(createdById)
-        
+    
     mostCommonUsers = Most_Common(createdByIds)
     
     if len(mostCommonUsers) == 0:
         return { 'Error': "No Trending Topics." }
-        
+    
     activeUsers = ""
     
     activeUserTopics = []
@@ -123,7 +123,7 @@ def next():
                 fouthTopicIds.append(topicId)
             elif createdById == fifth:
                 fifthTopicIds.append(topicId)
-                
+        
         firstMostCommon = Most_Common_Topic(firstTopicIds)
         activeUserTopics.append({ "{0}".format(first): firstMostCommon[0][0] }).format(first)
         
@@ -140,7 +140,7 @@ def next():
         activeUserTopics.insert({ "{0}": fifthMostCommon[0][0] }).format(fifth)
         
         activeUsers = "('{0}','{1}','{2}','{3}','{4}')".format(first, second, third, fouth, fifth)
-            
+    
     elif len(mostCommonUsers) == 4:
         
         first = mostCommonUsers[0][0]
@@ -166,7 +166,7 @@ def next():
                 thirdTopicIds.append(topicId)
             elif createdById == fouth:
                 fouthTopicIds.append(topicId)
-                
+        
         firstMostCommon = Most_Common_Topic(firstTopicIds)
         activeUserTopics.insert({ "{0}": firstMostCommon[0][0] }).format(first)
         
@@ -178,9 +178,9 @@ def next():
         
         fouthMostCommon = Most_Common_Topic(fouthTopicIds)
         activeUserTopics.insert({ "{0}": fouthMostCommon[0][0] }).format(fouth)
-
+        
         activeUsers = "('{0}','{1}','{2}','{3}')".format(first, second, third, fouth)
-            
+    
     elif len(mostCommonUsers) == 3:
         
         first = mostCommonUsers[0][0]
@@ -201,7 +201,7 @@ def next():
                 secondTopicIds.append(topicId)
             elif createdById == third:
                 thirdTopicIds.append(topicId)
-                
+        
         firstMostCommon = Most_Common_Topic(firstTopicIds)
         activeUserTopics.insert({ "{0}": firstMostCommon[0][0] }).format(first)
         
@@ -210,9 +210,9 @@ def next():
         
         thirdMostCommon = Most_Common_Topic(thirdTopicIds)
         activeUserTopics.insert({ "{0}": thirdMostCommon[0][0] }).format(third)
-
+        
         activeUsers = "('{0}','{1}','{2}')".format(first, second, third)
-            
+    
     elif len(mostCommonUsers) == 2:
         
         first = mostCommonUsers[0][0]
@@ -228,7 +228,7 @@ def next():
                 firstTopicIds.append(topicId)
             elif createdById == second:
                 secondTopicIds.append(topicId)
-                
+        
         firstMostCommon = Most_Common_Topic(firstTopicIds)
         activeUserTopics.insert({ "{0}": firstMostCommon[0][0] }).format(first)
         
@@ -236,7 +236,7 @@ def next():
         activeUserTopics.insert({ "{0}": secondMostCommon[0][0] }).format(second)
         
         activeUsers = "('{0}', '{1}')".format(first, second)
-            
+    
     elif len(mostCommonUsers) == 1:
         
         first = mostCommonUsers[0][0]
@@ -246,30 +246,30 @@ def next():
             topicId = row[0]
             if createdById == first:
                 firstTopicIds.append(topicId)
-                
+        
         firstMostCommon = Most_Common_Topic(firstTopicIds)
         # return { 'HERE': firstMostCommon}
         # activeUserTopics["{0}"format(first)].append("{0}".format(firstMostCommon[0][0]))
         activeUserTopics.append({ '{}'.format(first): firstMostCommon })
         activeUsers = "('{0}')".format(first)
-
-        
+    
+    
     activeUsersCommand = "SELECT u.id, u.displayName, u.email, u.isGold, u.isVerified, u.username, u.type, p.playCount FROM GOSSUP.user u, GOSSUP.profile p WHERE u.id=p.id AND u.id IN {0} GROUP BY u.id, u.displayName, u.email, u.isGold, u.isVerified, u.username, u.type, p.playCount ORDER BY p.playCount LIMIT 5 OFFSET 0;".format(activeUsers)
-
+    
     getUsersSqlCommand = {
         'commands': activeUsersCommand,
         'limit': 5,
         'separator': ";",
         'stop_on_error': "yes"
     }
-        
+    
     conn.request("POST", "/dbapi/v4/sql_jobs", headers=headers, body=json.dumps(getUsersSqlCommand))
-        
+    
     userRes = conn.getresponse()
     userData = userRes.read()
     
     userTransactionID = json.loads(userData.decode("utf-8")).get('id')
-        
+    
     conn.request("GET", "/dbapi/v4/sql_jobs/{}".format(userTransactionID), headers=headers)
     
     userGetRes = conn.getresponse()
