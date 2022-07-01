5656c71e910d9a278779030b85185ad360e000a0
Billy Askea <billy@gossup.com>
2022-06-30 23:32:57 -0700

    89

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
-import os
-import sys
-import json
-import http.client
-from collections import Counter
-from flask import Flask, render_template, request, url_for, jsonify
+from flask import Flask
+from threading import Thread
+
+
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def main():
-    return { 'message': "Hello" }
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+
+def home():
+
+    return f"<h1>😎I'm Awake Already!🔥</h1>"
+
+def run():
+
+  app.run()
+
+
+
+def keep_alive():
+
+    t = Thread(target=run)
+
+    t.start()
+
+keep_alive()
