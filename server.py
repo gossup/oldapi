d57c556f6df31743e9f591c6a406de91fc8b945d
Billy Askea <billy@gossup.com>
2022-07-02 18:41:21 -0700

    101

--- server.py
+++ server.py
@@ -1,52 +1,55 @@
 import os
+import sys
 import json
-import time
-import logging
-import requests
-from requests.auth import HTTPBasicAuth
-from flask import Flask
+import http.client
+from collections import Counter
+from flask import Flask, render_template, request, url_for, jsonify
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.DEBUG)
-start_time = time.time()
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    token = request.args.get('token')
+    if not token:
+        return { 'message': "Missing token." }
+    name = request.json['name']
+    if not name:
+        return { 'message': "Missing name." }
+    return { 'message': name }
 
-@app.route('/')
-def get_venmo_data():
-  session = requests.Session()
-  url = "https://venmo.com/api/v5/public?limit={}"
-  for i in range(50):
-    response = session.get(url.format(1))
-    response_dict = json.loads(response.text)
-    for transaction in response_dict["data"]:
-      print(unicode(transaction["message"]))
-    url = response_dict["paging"]["next"] + "&limit={}"
+@app.route('/next', methods=['POST', 'GET'])
+def next():
+    token = request.args.get('token')
+    if not token:
+        return { 'message': "Missing token." }
+    return { 'message': token }
 
-if __name__ == "__main__":
+@app.route('/again', methods=['POST', 'GET'])
+def again():
+    depID = os.getenv('depID')
+    if not depID:
+        return { 'message': "Missing depID." }
+    hostname = os.getenv('db2-hostname')
+    if not hostname:
+        return { 'message': "Missing hostname." }
+    token = request.args.get('token')
+    if not token:
+        return { 'message': "Missing token." }
+    name = request.json['name']
+    if not name:
+        return { 'message': "Missing name." }
+    return { 'message': name }
+
+@app.route('/andagain', methods=['POST', 'GET'])
+def andagain():
+    input_json = request.get_json(force=True)
+    return input_json
+
+if __name__ == '__main__':
     app.run()
 
 
-#from flask import Flask
-#from flask_db2 import DB2
-#
-#app = Flask(__name__)
-#
-#app.config['DB2_DATABASE'] = 'sample'
-#app.config['DB2_HOSTNAME'] = 'localhost'
-#app.config['DB2_PORT'] = 50000
-#app.config['DB2_PROTOCOL'] = 'TCPIP'
-#app.config['DB2_USER'] = 'db2inst1'
-#app.config['DB2_PASSWORD'] = 'db2inst1'
-#
-#db = DB2(app) #You forgot that
-#
-#
-#@app.route('/')
-#def index():
-#    cur = db.connection.cursor()
-#    return cur.execute("")
-
-#from flask import Flask
+#mport Flask
 #from threading import Thread
 #import sys
 #import ibm_db
